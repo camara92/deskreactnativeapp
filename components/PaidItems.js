0210afe666211fa2c7a00e985f6e1f4f71eef0ee
camara92 <88945535+camara92@users.noreply.github.com>
2022-08-04 10:04:58 +0200

    maj

--- components/PaidItems.js
+++ components/PaidItems.js
@@ -1,31 +1,35 @@
-import {Button, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React, {useState} from 'react';
 import globalStyles from '../styles/globalStyles';
 // import { Icon } from 'react-native-vector-icons/icon';
-import {Icon }from 'react-native-vector-icons/FontAwesome';
+import {Icon} from 'react-native-vector-icons/FontAwesome';
 import CoursesOverview from './CoursesOverview';
 
-const PaidItems = ({  date,title, courseDetails }) => {
-
+const PaidItems = ({date, title, courseDetails}) => {
   const [isShowing, setisShowing] = useState(false);
 
-  const handleShow=()=>{
-    setisShowing(prevState =>!prevState)
-  }
- 
+  const handleShow = () => {
+    setisShowing(prevState => !prevState);
+  };
+
   return (
     <ScrollView style={styles.paidCoursesContainer}>
       <View style={styles.paidCourses}>
-        <Text style={styles.title}>Réservation  {title} </Text> 
-        <Text style={styles.date}> { date} </Text>
+        <Text style={styles.title}>Réservation {title} </Text>
+        <Text style={styles.date}> {date} </Text>
       </View>
       <TouchableOpacity
         styele={styles.iconbtn}
         style={styles.btnclick}
-              
-              color="black"
-               onPress={handleShow}
-      >
+        color="black"
+        onPress={handleShow}>
         {/* <Button
               style={styles.btnclick}
               title={isShowing ? "-" : "+"} 
@@ -34,31 +38,17 @@ const PaidItems = ({  date,title, courseDetails }) => {
               // name={isShowing ? "moins" : "plus"} 
               
             /> */}
-        
-          
-  
-
       </TouchableOpacity>
-      {
-        isShowing &&  (
-          <View style={styles.detailPaidCourses}>
-            {
-              courseDetails.courses.map( (courses)=>(
-                <CoursesOverview 
-                  key={courses.id}
-                  title={courses.title}
-                  // price={courses.price}
-
-
-                />
-              ))
-            }
-          </View>
-        )
-      }
+      {isShowing && (
+        <View style={styles.detailPaidCourses}>
+          {courseDetails.courses.map(courses => (
+            <CoursesOverview key={courses.id} title={courses.title} />
+          ))}
+        </View>
+      )}
     </ScrollView>
-  )
-}
+  );
+};
 
 export default PaidItems;
 
@@ -68,8 +58,6 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     margin: 10,
     padding: 15,
-  
-   
   },
   paidCourses: {
     flexDirection: 'row',
@@ -78,28 +66,26 @@ const styles = StyleSheet.create({
     width: '100%',
     marginBottom: 15,
   },
-  total:{
-    fontSize:18, 
-
-  }, 
-  date:{
-    fontSize:16
-  }, 
-  iconbtn:{
-    alignSelf:"flex-end"
+  total: {
+    fontSize: 18,
+  },
+  date: {
+    fontSize: 16,
+  },
+  iconbtn: {
+    alignSelf: 'flex-end',
   },
 
-  title:{
-    color: "red", 
-    
+  title: {
+    color: 'red',
   },
-  btnclick:{
-    flexDirection:"column", 
-    width:"10%"
-  }, 
-  detailPaidCourses:{
-    marginTop:15,
-    borderTopColor:globalStyles.lightGrey, 
-    borderTopWidth: 1
-  }
-})
+  btnclick: {
+    flexDirection: 'column',
+    width: '10%',
+  },
+  detailPaidCourses: {
+    marginTop: 15,
+    borderTopColor: globalStyles.lightGrey,
+    borderTopWidth: 1,
+  },
+});
